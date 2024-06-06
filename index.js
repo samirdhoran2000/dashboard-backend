13cb96136fcfa9c1fa46357cba32b44425d96a0f
SAMIR DHORAN <samirdhoran2000@gmail.com>
2024-06-06 13:44:21 +0530

    cors and api chage

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from "express";
 // import db from "./config/db.config.js";
+import cors from "cors"
 import exportRouter from "./routes/export.routes.js";
 
 const app = express();
 const PORT = process.env.port || 3000;
 
+app.use(cors({
+  origin:"*"
+}));
 app.use(express.json());
-app.use("/exportData", exportRouter);
+app.use("/api/exportData", exportRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
