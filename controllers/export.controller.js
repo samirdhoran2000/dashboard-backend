727afa844918066511fe627bb9bff227b949481b
SAMIR DHORAN <samirdhoran2000@gmail.com>
2024-06-07 16:09:59 +0530

    get average data

--- controllers/export.controller.js
+++ controllers/export.controller.js
@@ -2,7 +2,7 @@ import db from "../config/db.config.js";
 
 const getExportData = async (req, res) => {
   try {
-    const { rows, fields } = await db("select * from cosmo_table1 limit 1000");
+    const { rows, fields } = await db("select * from cosmo_table1");
 
     res.json({
       msg: "data get successfully ",
@@ -19,27 +19,24 @@ const getAverageData = async (req, res) => {
     const { country } = req.query;
     console.log(country);
     if (country) {
-      const { rows, fields } = await db(
-        `select  consignee_name, avg(quantity), avg(unit_fob_inr), count(consignee_name) from cosmoDB.cosmo_table1 where foreign_country='${country}' group by consignee_name`
-      );
-      res.json({
-        msg: "data get successfully ",
-        result: {
-          rows,
-        },
-      });
-    } else {
-      const { rows, fields } = await db(
-        `select  consignee_name, avg(quantity), avg(unit_fob_inr), count(consignee_name) from cosmoDB.cosmo_table1 group by consignee_name limit 1000`
-      );
+    const { rows, fields } = await db(
+      `SELECT
+    foreign_country,
+    AVG(total_fob_inr / quantity) AS average_price,
+    AVG(quantity) AS average_quantity,
+    COUNT(DISTINCT consignee_name) AS consignee_count
+FROM
+    cosmoDB.cosmo_table1
+GROUP BY
+    foreign_country `
+    );
 
-      res.json({
-        msg: "data get successfully ",
-        result: {
+    res.json({
+      msg: "data get successfully ",
+      result: {
           rows,
-        },
-      });
-    }
+      },
+    });
   } catch (error) {
     console.log(
       "something went wrong in get average data controller : ",
